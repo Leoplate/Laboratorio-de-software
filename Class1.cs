b1af485d074acf2109bc957e1c79f76926e07fc0
Leoplate <127459534+Leoplate@users.noreply.github.com>
2024-05-07 20:36:36 -0300

    Examen de pruebas

---
+++ Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit;
+namespace TestProject1
+{
+    
+    
+    public class Pruebas
+    {
+        [Fact]
+        public void PruebaSumar()
+        {
+            //Arrange
+               primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico P = new primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico();
+            //Act
+            int primer = 1;
+            int seg = 1;
+            int resu = P.Sumar(primer, seg);
+
+            //Assert
+            Assert.Equal(2, resu);
+
+        }
+
+        [Fact]
+        public void PruebaMayor()
+        {
+            //Arrange
+            primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico P = new primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico();
+            //Act
+            int primer = 1;
+            int seg = 1;
+            int resu = P.CalcularMayor(primer, seg);
+
+            //Assert
+            Assert.Equal(1, resu);
+
+        }
+
+
+        [Fact]
+        public void PruebaDividir()
+        {
+            //Arrange
+            primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico P = new primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico();
+            //Act
+            int primer = 1;
+            int seg = 1;
+            int resu = P.Dividir(primer, seg);
+
+            //Assert
+            Assert.Equal(1, resu);
+
+        }
+
+
+        [Fact]
+        public void PruebaNegativo()
+        {
+            //Arrange
+            primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico P = new primer_parcial_unit_test_procesador_matematico.ProcesadorMatematico();
+            //Act
+            int primer = -5;
+            int seg = -6;
+            int resu1 = P.CalcularMayor(primer, seg);
+            bool resu = P.EsNegativo();
+
+            //Assert
+            Assert.Equal(true, resu);
+
+        }
+
+    }
+}
